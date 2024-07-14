a698a8c70c3050881aa504a7f159e540a01892cc
Mehtap Demir <mehtap.demir011@gmail.com>
2024-07-14 14:11:43 +0300

    EfEntityRepositoryBase Applied

--- DataAccess/Concrete/EntityFramework/EfColourDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColourDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -10,54 +11,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfColourDal : IColourDal
+    public class EfColourDal : EfEntityRepositoryBase<Colour , RentACarContext>, IColourDal
     {
-        public void Add(Colour entity)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-
-
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Colour entity)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-        public void Update(Colour entity)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-        public List<Colour> GetAll(Expression<Func<Colour, bool>> filter = null)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                return filter == null ? context.Set<Colour>().ToList() : context.Set<Colour>().Where(filter).ToList();
-                //return colors;
-            }
-        }
-
-        public Colour Get(Expression<Func<Colour, bool>> filter)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                return context.Colours.Where(filter).First();
-            }
-        }
 
     }
 }
--- Entities/Concrete/Colour.cs
+++ Entities/Concrete/Colour.cs
@@ -1,9 +1,10 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Entities.Concrete
 {
